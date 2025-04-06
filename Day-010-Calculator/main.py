dbe9d736dc52e5ab544069aef990eb7d78dce7a1
Ashish Bamniya <ashishbam07@gmail.com>
2025-04-06 20:23:39 +0530

    Update main.py

--- Day-010-Calculator/main.py
+++ Day-010-Calculator/main.py
@@ -1,64 +1,93 @@
 from art import logo
 
+"""this function will start the Calculator"""
+def start_calculator():
 
-def add(num1, num2):
-    return num1 + num2
-  
-def sub(num1, num2):
-    return num1 - num2
+    print(logo)
+
+    def add(num1, num2):
+        sum = num1 + num2
+        print(f"{num1} + {num2} = {sum}")
+        return sum
+
+    def subtract(num1,num2):
+        sub = num1 - num2
+        print(f"{num1} + {num2} = {sub}")
+        return sub
+
+    def multiply(num1,num2):
+        mul = num1 * num2
+        print(f"{num1} + {num2} = {mul}")
+        return mul
+
+    def divide(num1,num2):
+        div = num1 / num2
+        print(f"{num1} + {num2} = {div}")
+        return div
+
+    calculation_operators = {"+": add,
+                            "-": subtract,
+                            "*": multiply,
+                            "/": divide
+                            }
+
+    #to stop avoiding unbound local variable error, passed a variable in
+    # a restart_calculator function so don't get error
+    n1 = float(input("What's the First number? :"))
+
+    """This function will restart the Calculator """
+    def restart_calculator(n1):
+
+        #this for loop will print out operators which we want to perform
+        for to_print_operators in calculation_operators:
+            print(to_print_operators)
+
+        operations = input("Pick an Operation: ")
+        n2 = float(input("What's the next number? :"))
+
+        #here from dictionary 'key' which are operators are being checked that which calculation operation
+        #user wants to perform and as per that operation will be performed.
+        for operator in calculation_operators:
+            if operations == operator:
+
+                #here we are accessing function operators like(func add) and also giving them the value
+                #to perform on which number they should perform calculation.
+                calc_result  = calculation_operators[operator](n1, n2)
+
+                """here if the user gives the wrong input then this while loop runs until the user don't provide
+                the correct input"""
+                correct_user_input = True
+                while correct_user_input:
+                    continue_calculation = input(f"Type 'y' to continue calculating with {calc_result} "
+                                                f"or type 'n' to start a new calculation or type 'exit' to "
+                                                 f"exit the program:").lower()
+
+                    if continue_calculation == "y":
+                        #here calculated value is being assigned to n1 variable
+                        n1 = calc_result
+                        restart_calculator(n1)
+
+                    elif continue_calculation == "n":
+                        print("\n" * 17)
+                        start_calculator()
+
+                    elif continue_calculation == "exit":
+                        print("Good Bye!")
+                        exit()
+
+                    else:
+                        print("Invalid Input, Please Provide correct Input!\n")
+
+    restart_calculator(n1)
     
-def mul(num1, num2):
-    return num1 * num2
-    
-def div(num1, num2):
-    return num1 / num2
+start_calculator()
 
 
 
 
-Operations = {"+" : add, "-" : sub, "*" : mul, "/" : div}
 
-def Calculator():
-  print(logo)
 
-  is_true = True
-  n1 = float(input("What's the first number: "))
-  
-  while is_true == True:
-    
-    n2 = float(input("What's the Second number: "))
-    
-    for op in Operations:
-      print(op)
-  
-    operation_sym = input("Pick an operation form the above line : ")
-    
-    if operation_sym == "+" or operation_sym == "-" or operation_sym == "*" or operation_sym == "/":
-        
-      Calculation_function = Operations[operation_sym]
-      
-      answer = Calculation_function(num1 = n1, num2 = n2)
-  
-      print(f"{n1} {operation_sym} {n2} = {round(answer, 2)}" )
-  
-    
-      is_continue = input("do you want to continue then type Yes and No to exit or 'Restart' for new calculation : ").title()
-    
-      if is_continue == "Yes":
-        n1 = answer
-        is_true = True
-    
-      elif is_continue == "No":
-        is_true = False
 
-      elif is_continue == "Restart":
-        Calculator()
-    
-      else:
-        print("Invalid Input !")
-  
-    else:
-      print("invalid symbol !")
 
-Calculator()
-    
+
+
