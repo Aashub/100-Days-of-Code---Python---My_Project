4c58eedd3129e00a2ca4906cd889a70db6e3c870
Ashish Bamniya <ashishbam07@gmail.com>
2025-05-25 14:26:23 +0530

    Update scoreboard.py

--- Day-021-Pong_Game/scoreboard.py
+++ Day-021-Pong_Game/scoreboard.py
@@ -1,29 +1,26 @@
-from turtle import Turtle
+from turtle import  Turtle
 
 
-class Scoreboard(Turtle):
+class Score(Turtle):
+    """here we are inheriting from turtle class to create our Score class and use its methods and attribute"""
 
     def __init__(self):
         super().__init__()
-        self.color("white")
+
         self.penup()
+        self.color("white")
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
-        self.update_scoreboard()
+
+        self.update_screen()
 
 
-    def update_scoreboard(self):
+    def update_screen(self):
+        """this function will align the score points in a specific location"""
         self.clear()
-        self.goto(-100, 250)
-        self.write(self.l_score, align="center", font=("Arial", 30, "normal"))
-        self.goto(100, 250)
-        self.write(self.r_score, align="center", font=("Arial", 30, "normal"))
+        self.goto(50, 245)
+        self.write(self.l_score, move=False, font=("Arial", 40, "normal"))
+        self.goto(-50, 245)
+        self.write(self.r_score, move=False, font=("Arial", 40, "normal"))
 
-    def L_score(self):
-        self.l_score += 1
-        self.update_scoreboard()
-
-    def R_score(self):
-        self.r_score += 1
-        self.update_scoreboard()
