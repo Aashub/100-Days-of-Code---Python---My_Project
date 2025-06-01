368dda94f3851caa8e7ca3c4e811b43e02fc1d39
Ashish Bamniya <ashishbam07@gmail.com>
2025-06-01 20:13:47 +0530

    Update score.py

--- Day-023-Snake_Game_Higest_Score_Update/score.py
+++ Day-023-Snake_Game_Higest_Score_Update/score.py
@@ -1,32 +1,32 @@
-from turtle import Turtle
-align = "center"
-font = ("Arial",13, "bold")
-movement = False
-
+from turtle import  Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 15, "normal")
 class Score(Turtle):
-    
+    """the main purpose of this class is to update the score whenever snakes eat a food"""
+
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.penup()
+        self.increase_score = 0
+        with open("data.txt") as data:
+            self.highest_score = int(data.read())
+        self.score_card()
+        self.reset_score()
+
+
+    def reset_score(self):
+        """this function is reset hte current score value and also update the highest score value."""
+        if self.increase_score > self.highest_score:
+            self.highest_score = self.increase_score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highest_score}")
+        self.increase_score = 0
+
+    def score_card(self):
+        """this function will align the position of score card in top of the screen."""
+        self.clear()
         self.color("white")
         self.hideturtle()
-        self.goto(x=0, y=230)
-        self.update_score_board()
-
-    def update_score_board(self):
-        self.write(arg=f"Score: {self.score}", align= align, font=font, move=movement)
-
-    def game_over(self):
-        self.goto(0,0)
-        self.color("white")
-        self.write(arg=f"Game Over", align="center", font=font)
-
-
-    def track_score(self):
-        self.score += 1
-        self.clear()
-        self.update_score_board()
-
-
+        self.penup()
+        self.goto(x=0, y=270)
+        self.write(f"Score: {self.increase_score}   Highest Score: {self.highest_score}", True, align=ALIGNMENT, font= FONT)
 
