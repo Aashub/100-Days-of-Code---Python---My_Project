210038aa7705a18d055c266e6927eed89c0f3550
Ashish Bamniya <ashishbam07@gmail.com>
2025-06-10 23:11:41 +0530

    Update main.py

--- Day-024-Mail_Merge/main.py
+++ Day-024-Mail_Merge/main.py
@@ -1,15 +1,50 @@
 
-PLACE_HOLDER = "[name]"
+#empty list to store /n removed names
+name_list = []
 
-with open("../Mail Merge Project Start/Input/Names/invited_names.txt") as names:
-    invited_name =  names.readlines()
+# here we are reading the file of starting_letter
+with open("./Input/Letters/._starting_letter.txt") as starting_letter:
+    draft_letter = starting_letter.read()
 
-with open("../Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter:
-    letter_content = letter.read()
-    for name in invited_name:
-        striped_name = name.strip()
-        replaced_content = letter_content.replace(PLACE_HOLDER, striped_name)
-        print(replaced_content)
-        with open(f"../Mail Merge Project Start/Output/ReadyToSend/letter_{striped_name}.txt" , mode = "w") as create_letter:
-            create_letter.write(replaced_content)
+    #from the invited_names file we are reading here each line and storing its value in "name"
+    names = open("./Input/Names/._invited_names.txt")
+    name = names.readlines()
+
+    # by using for loop we have appended the invited names in name_list and stripping "/n" which is making difficult to
+    #read or write the file
+    for name_of_list in name:
+        name_list.append(name_of_list.strip())
+    names.close()
+
+    # here we have created a new files for the invited names so we can create a letter.
+    for creating_files in name_list:
+        with open(f"./Output/ReadyToSend/{creating_files}.txt", "w") as created_files:
+            file = created_files.write(draft_letter)
+
+    for named_letter in name_list:
+
+        # here we are opening the newly created letter in read mode so we replace the "name" with invited names
+        with open(f"./Output/ReadyToSend/{named_letter}.txt", "r") as new_files:
+            file_name = new_files.read()
+            new = file_name.replace("[name]", named_letter)
+
+        # here we are opening the newly created letter in write mode so the invited names will updated in each file
+        with open(f"./Output/ReadyToSend/{named_letter}.txt", "w") as updated_files:
+            new_file = updated_files.write(new)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
