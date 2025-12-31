f326eb1ac82456c5a3dcd40de58f0828e82ecc84
Ashish Bamniya <ashishbam07@gmail.com>
2025-12-31 13:42:07 +0530

    Update scoreboard.py

--- Day-020-021-Snacke_Game/scoreboard.py
+++ Day-020-021-Snacke_Game/scoreboard.py
@@ -5,24 +5,42 @@ class Score(Turtle):
 
     def __init__(self):
         super().__init__()
+
+        # this will open the data file and read the content stored in it.
+        with open(file="data.txt", mode= "r") as file:
+            self.high_score = int(file.read())
+
         self.score = 0
         self.penup()
         self.hideturtle()
         self.color("white")
         self.goto(0,278)
-        self.increase_score()
         self.boarder_line()
+        self.update_scoreboard()
 
-    def write_game_over(self):
-        """this method will write game over in the screen."""
 
-        self.goto(0, 0)
-        self.write(arg=f"Game Over!", align="center", font=('Courier', 15, 'normal'))
+    def reset_scoreboard(self):
+
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+            # this will update the new highest score in the dat.txt file and when next time someone reads it that will appear as high score.
+            with open(file="data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", align="center",
+                   font=('Courier', 15, 'normal'))
 
     def increase_score(self):
         """this method will help to increase the score whenever snake eat the food"""
-        self.write(arg=f"Score: {self.score}", align="center", font=('Courier', 15, 'normal'))
+
         self.score += 1
+        self.update_scoreboard()
 
 
     def boarder_line(self):
