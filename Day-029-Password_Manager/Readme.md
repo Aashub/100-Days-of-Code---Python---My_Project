9bca15357d615e9e6cea31d273fc0883c11acad2
Ashish Bamniya <ashishbam07@gmail.com>
2026-01-09 14:49:41 +0530

    initial commit

--- Day-029-Password_Manager/Readme.md
+++ Day-029-Password_Manager/Readme.md
@@ -1,7 +1,7 @@
-# Day 29 - Password Manager GUI App
+# Day 29 - 30 - Password Manager GUI App
 
 ## Project Overview
- This project is a **Password Manager** app using **Python’s Tkinter library**, which introduced me to building real-world GUI applications that are not only interactive but also practical for daily use. In this  **Password Manager GUI App** user can Generate **strong, complex passwords** that are hard to crack and Save those passwords along with the **website/platform name** and **email/username** in a `.txt` file so they don't have to be remembered and can use them later. In order to create this project i have learned about how to use pyperclip library for auto copy the generated password, Tkinter class, revised and implemented how to use list comprehension and file handling in python  
+ This project is a **Password Manager** app using **Python’s Tkinter library**, which introduced me to building real-world GUI applications that are not only interactive but also practical for daily use. In this  **Password Manager GUI App** user can Generate **strong, complex passwords** that are hard to crack and Save those passwords along with the **website/platform name** and **email/username** in a `.json` file so they don't have to be remembered and can use them later and they can also search the email address and password using serarch button if they want to look into that password. In order to create this project i have learned about how to use pyperclip library for auto copy the generated password, Tkinter class, revised and implemented how to use list comprehension and file handling in python and learned about how to read, write and update data in `.json` file, & how to do error handling using try, except, else, finally block of code and also learned about how to raise custom error using raise block.  
 
 ---
 
@@ -15,6 +15,12 @@
 
 * Also I have revised and implemented the use of list comprehensions for generating a mix of letters, symbols, and numbers to create a password. Then I learned ow to use .join() to combine them into one string. I also learned how to automatically copy the password to clipboard using pyperclip.copy().
 
+* by building this project I have learned about how to read(`.load()`), write(`.dump()`), append(`.update()`) data in existing json file and also create a new .json file if it doesn't exist automatically.
+
+* Also I have learned about `try, except, else, and finally` block of code to handle error where on certain situation programs give us an error if somethign will go wrong for example in this program when the user runs the program first time the .jason file is not exist so this try except block of code execute and create a new file so that users data get stored in the file.
+
+* Besides that I also learned about how to raise a custom error in certain situation using `raise` keyword
+
 ---
 
 ## How It Works 
@@ -25,14 +31,22 @@
 
 * **Generating a Password**: When the **Generate Password** button is clicked A strong password is created using `generate_password` function that randoms letters, numbers, and symbols via list comprehension and created a strong password. The password is displayed in the password field and automatically copied to the clipboard to use it.
 
-* **Validation & Saving the Data**: If any input field is empty and the user tries to save, a **pop-up alert** appears saying not to leave fields empty. When all fields are filled and the **Add** button is clicked a confirmation pop-up appears showing all the input details. If the user clicks OK, the details are saved to a `data.txt` file. The input fields are then cleared for the next entry.
+* **Validation & Saving the Data**: If any input field is empty and the user tries to save, a **pop-up alert** appears saying not to leave fields empty. When all fields are filled and the **Add** button is clicked a confirmation pop-up appears showing all the input details. If the user clicks OK, the details are saved to a `data.json` file. The input fields are then cleared for the next entry.
 
+* **JSON File Handling**:The password details are stored in a data.json file using dictionary format. If the file does not exist, it is automatically created; otherwise, the new data is merged with the existing data.  
+
+* **Exception Handling**: A try-except-else-finally block is used to safely handle file operations in case the file does not exist than this block of code will prevent the program from crashing when the data file is missing and create a new file.
+
+* **Searching Saved Passwords**: The Search button allows users to retrieve saved credentials. If the website exists in the JSON file, the email and password are shown in a pop-up and the password is copied to the clipboard. If not found or if the file is missing, an appropriate alert message is displayed.
 ## Project Highlights
 
 * Built the GUI application
 * Revised how to use list comprehension.
 * learned how to use, User-friendly field validation and pop-up messages.
-* Revised myself about how open() and write() a file. 
+* Revised myself about how open() and write() a file.
+* learned about how to do error handling using try, except, else, and final block of code
+* learned about how to raise a custom error.
+* learned about how to read, write and update data in .json file.
 * Real-world utility app that actually helps manage passwords.
 
 ---
--- Day-029-Password_Manager/main.py
+++ Day-029-Password_Manager/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import pyperclip
+import json
 
 import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -32,7 +33,6 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-
 def save():
     """this function will save the password in the txt file."""
 
@@ -40,25 +40,67 @@ def save():
     email_username =email_username_input_field.get()
     password = password_input_field.get()
 
+    new_data = {
+        website_name: {
+
+            "email": email_username,
+            "password": password
+        }
+    }
 
     if len(website_name) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message= "Please don't leave any fields empty!")
 
     else:
-        is_ok = messagebox.askokcancel(title=website_name, message= f"These are the details entered: \nEmail: {email_username}"
-                                                                f" \nPassword: {password} \nDo you want to say this details?")
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website_name} | {email_username} | {password}\n")
+        # this try except block handle if first time program will run and their is no file to read it so except block create the json file.
+        try:
+            with open("data.json", "r") as file:
+                # reading the old data
+                data = json.load(file)
 
-            if email_username != "ashishbam07@gmail.com":
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+
+        else:
+            # updating the old data new data
+            data.update(new_data)
+            # saving updated data
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+
+        finally:
+            if email_username != "anony@gmail.com":
                 email_username_input_field.delete(0, "end")
-                email_username_input_field.insert(0, "ashishbam07@gmail.com")
+                email_username_input_field.insert(0, "anony@gmail.com")
 
             website_input_field.delete(0, "end")
             password_input_field.delete(0, "end")
-
             website_input_field.focus()
+
+# ---------------------------- Search Data ------------------------------- #
+
+def find_password():
+    """this function will look for the saved old data and and show the email password of website which user has searcehd."""
+
+    searched_entry = website_input_field.get().lower()
+
+    try:
+        with open("data.json", "r") as file:
+            # reading the old data
+            old_data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title=f"Oops", message= "No Data File Found!")
+
+    else:
+        if searched_entry in old_data:
+            email = old_data[searched_entry]["email"]
+            password = old_data[searched_entry]["password"]
+            pyperclip.copy(password)
+            messagebox.showinfo(title=f"{searched_entry}", message= f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title=f"Hmm", message=f"No details for {searched_entry} website exists")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -84,7 +126,7 @@ canvas.grid(row=0, column= 1, sticky="w")
 website_label = Label(text="Website: ", font = FONT)
 website_label.grid(row= 1, column=0 , sticky= "w")
 
-website_input_field = Entry(width=39, justify="left", font=FONT)
+website_input_field = Entry(width=21, justify="left", font=FONT)
 website_input_field.grid(row=1, column=1, pady=2, columnspan=2, sticky= "w")
 website_input_field.focus()
 
@@ -92,8 +134,8 @@ email_username_label = Label(text="Email/Username: ", font = FONT)
 email_username_label.grid(row= 2, column=0 , sticky= "w")
 
 email_username_input_field = Entry(width=39, justify="left", font=FONT)
-email_username_input_field.grid(row=2, column=1,pady=2, columnspan=2, sticky= "w")
-email_username_input_field.insert(0, "anony@@gmail.com")
+email_username_input_field.grid(row=2, column=1,pady=3, columnspan=2, sticky= "w")
+email_username_input_field.insert(0, "anony@gmail.com")
 
 password_label = Label(text="Password: ", font = FONT)
 password_label.grid(row= 3, column=0 , sticky= "w")
@@ -101,11 +143,15 @@ password_label.grid(row= 3, column=0 , sticky= "w")
 password_input_field = Entry(width=21, font=FONT)
 password_input_field.grid(row=3, column=1, columnspan=2, pady=2, sticky= "w")
 
-generate_pass_button = Button(text="Generate Password", font = BUTTON_FONT, command = generate_password)
-generate_pass_button.place(x = 274, y = 252)
+search_button = Button(text="Search", font = BUTTON_FONT, command = find_password, highlightthickness=1)
+search_button.place(x = 273, y = 205)
+search_button.config(width=16)
+
+generate_pass_button = Button(text="Generate Password", font = BUTTON_FONT, command = generate_password, highlightthickness=1)
+generate_pass_button.place(x = 273, y = 255)
 
 
-add_button = Button(text="Add", width= 38, font = BUTTON_FONT, command= save)
-add_button.grid(row=4, column=1, columnspan=2,pady= 2,  sticky= "w")
+add_button = Button(text="Add", width= 38, font = BUTTON_FONT, command= save, highlightthickness=1)
+add_button.grid(row=4, column=1, columnspan=2,pady= 3,  sticky= "w")
 
 window.mainloop()
